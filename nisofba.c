7e2e8be687d894d57344de95e6d0f1d81f5ac12e
niteraleph <niteraleph@protonmail.com>
2017-09-06 05:55:02 +0200

    fixed a couple of things, operation start
    started writing code for choosing operations from command line

--- nisofba.c
+++ nisofba.c
@@ -9,17 +9,19 @@ int i;
 unsigned long l;
 char source_path[] = "./example2.txt";
 char destination_path[] = "./backup_file.txt";
+char add_operation[] = {'a','d','d'};
 char *_current_file_content;
 char chosen_operation[100];
 char *buffer_file;
 off_t source_size;
-struct stat stbuf;
+struct stat stbuf; //consider renaming
 
 
 void readConsoleArguments();
 void readFile();
 void addToList();
 void createBackupFile();
+int compare_char_arr(char array1[], char array2[], int length);
 
 
 
@@ -27,8 +29,6 @@ int main(int argc, char *argv[])
 {
 	readConsoleArguments(argc, argv);
 	printf("%s", chosen_operation);
-	addToList();
-	
 	readFile();
 	createBackupFile();  
 	return 0;
@@ -39,9 +39,11 @@ void readConsoleArguments(int argument_number, char *argumentContent[])
 	for(l =0; l <= sizeof(chosen_operation); l++)
 	{
 		chosen_operation[l] = argumentContent[1][l];
-
 	}
-
+	if(compare_char_arr(chosen_operation, add_operation, 3) == 1)
+		{
+			addToList();
+		}
 }
 
 void readFile()
@@ -52,7 +54,7 @@ void readFile()
 	file_to_read = fopen(source_path, "r");
 	fstat(file_to_read_descriptor, &stbuf);
 	source_size = stbuf.st_size;
-	buffer_file = (char*)malloc(source_size);
+	buffer_file = (char*)malloc(source_size);   ///FIX THE MALLOC. MUST DEALLOCATE
 	fread(buffer_file,sizeof(char),source_size, file_to_read);
 	fclose(file_to_read);
 	close(file_to_read_descriptor);
@@ -74,4 +76,27 @@ void addToList()
 	FILE *backup_list;
 	backup_list = fopen("./backup_list", "app");
 	fwrite(source_path, sizeof(char), sizeof(source_path)-1, backup_list);
+	fputc('\n', backup_list);
 }
+
+int compare_char_arr(char array1[], char array2[], int length)
+{
+	int is_the_same = 0;
+	for(i = 0; i < length; i++)
+	{
+		if(array1[i] == array2[i])
+		{
+			is_the_same = 1;
+		}
+		else
+		{
+			is_the_same = 0;
+			break;
+		}
+	}
+	return is_the_same;
+}
+
+
+
+
